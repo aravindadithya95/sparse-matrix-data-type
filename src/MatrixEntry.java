943ee1738af5ab96c85174b785f22c4526da6cbc
Aravind Adithya <aravindadithya95@gmail.com>
2018-03-07 18:54:31 -0500

    Cleanup

--- src/MatrixEntry.java
+++ src/MatrixEntry.java
@@ -1,25 +1,25 @@
 public class MatrixEntry {
 
-	private final int value;
-	
-	private final int row;
-	private final int col;
-	
-	public MatrixEntry(int value, int row, int col) {
-		this.value = value;
-		this.row = row;
-		this.col = col;
-	}
-	
-	public int getValue() {
-		return value;
-	}
-	
-	public int getRow() {
-		return row;
-	}
-	
-	public int getCol() {
-		return col;
-	}
+    private final int value;
+    
+    private final int row;
+    private final int col;
+    
+    public MatrixEntry(int value, int row, int col) {
+        this.value = value;
+        this.row = row;
+        this.col = col;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getCol() {
+        return col;
+    }
 }
